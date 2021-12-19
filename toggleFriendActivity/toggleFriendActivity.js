232930264117c81d972b0c10041d67fdfe65c9a1
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-12-19 21:39:17 +1100

    SUCK MY BALLS THIS TOOK TOO LONG

--- toggleFriendActivity/toggleFriendActivity.js
+++ toggleFriendActivity/toggleFriendActivity.js
@@ -38,7 +38,8 @@
   
   // UI Text
   const BUTTON_NAME_TEXT = "Toggle(Show/Hide) Friend Activity Tab";
-  const BUTTON_ICON = `<p style="font-size:8px">Activity</p>`
+  const BUTTON_ICON = `<?xml version="1.0" encoding="UTF-8"?>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`
   new Spicetify.Topbar.Button(
     BUTTON_NAME_TEXT,
     BUTTON_ICON,
